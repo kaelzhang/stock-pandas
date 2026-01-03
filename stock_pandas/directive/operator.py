2610e2c61ec343e7070ed6464c6c88000a6d3199
Kael Zhang <i+github@kael.me>
2026-01-03 19:52:19 +0800

    operator: all operators and priorities

--- stock_pandas/directive/operator.py
+++ stock_pandas/directive/operator.py
@@ -1,7 +1,136 @@
+from typing import (
+    Union,
+    Callable,
+    Any,
+    Dict
+)
+
 from stock_pandas.common import (
     compare_cross,
     NDArrayAny
 )
+
+from numpy.typing import NDArray
+
+
+ReturnType = NDArray[Any]
+OperatorArgType = Union[float, ReturnType]
+OperatorFormula = Callable[[OperatorArgType, OperatorArgType], ReturnType]
+UnaryOperatorFormula = Callable[[OperatorArgType], ReturnType]
+OperatorMap = Dict[str, OperatorFormula]
+UnaryOperatorMap = Dict[str, UnaryOperatorFormula]
+
+
+def logical_or(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left | right
+
+
+def logical_and(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left & right
+
+
+LOGICAL_OPERATORS: OperatorMap = {
+    '||': logical_or,
+    '&&': logical_and
+}
+
+
+def bitwise_or(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left | right
+
+
+BITWISE_OR_OPERATORS: OperatorMap = {
+    '|': bitwise_or,
+}
+
+
+def bitwise_xor(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left ^ right
+
+
+BITWISE_XOR_OPERATORS: OperatorMap = {
+    '^': bitwise_xor,
+}
+
+
+def bitwise_and(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left & right
+
+
+BITWISE_AND_OPERATORS: OperatorMap = {
+    '&': bitwise_and,
+}
+
+
+def less_than(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left < right
+
+
+def less_than_or_equal(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left <= right
+
+
+def larger_than_or_equal(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left >= right
+
+
+def larger_than(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left > right
+
+
+RELATIONAL_OPERATORS: OperatorMap = {
+    '<': less_than,
+    '<=': less_than_or_equal,
+    '>=': larger_than_or_equal,
+    '>': larger_than,
+}
+
+
+def equal(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left == right
+
+
+def not_equal(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left != right
+
+
+EQUALITY_OPERATORS: OperatorMap = {
+    '==': equal,
+    '!=': not_equal,
+}
 
 
 def cross(
@@ -28,81 +157,51 @@ def cross_down(
     return cross & ~ less
 
 
-def less_than(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left < right
-
-
-def less_than_or_equal(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left <= right
-
-
-def equal(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left == right
-
-
-def not_equal(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left != right
-
-
-def larger_than_or_equal(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left >= right
-
-
-def larger_than(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left > right
-
-
-def logical_or(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left | right
-
-
-def logical_and(
-    left: NDArrayAny,
-    right: NDArrayAny
-) -> NDArrayAny:
-    return left & right
-
-
-OPERATORS = {
+STYLE_OPERATORS: OperatorMap = {
     '//': cross_up,
     '\\': cross_down,
-    '><': cross,
-    '<': less_than,
-    '<=': less_than_or_equal,
-    '==': equal,
-    '!=': not_equal,
-    '>=': larger_than_or_equal,
-    '>': larger_than,
-    '|': logical_or,
-    '&': logical_and
+    '><': cross
 }
 
 
-# def plus(
-#     array: NDArrayAny
-# ) -> NDArrayAny:
-#     return array
+def addition(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left + right
+
+
+def subtraction(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left - right
+
+
+ADDITION_OPERATORS: OperatorMap = {
+    '+': addition,
+    '-': subtraction
+}
+
+
+def multiplication(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left * right
+
+
+def division(
+    left: NDArrayAny,
+    right: NDArrayAny
+) -> NDArrayAny:
+    return left / right
+
+
+MULTIPLICATION_OPERATORS: OperatorMap = {
+    '*': multiplication,
+    '/': division
+}
 
 
 def minus(
@@ -120,8 +219,7 @@ def not_operator(
 # + directive
 # - directive
 # ~ directive
-UNERY_OPERATORS = {
-    # '+': plus,
+UNARY_OPERATORS: UnaryOperatorMap = {
     '-': minus,
     '~': not_operator
 }
